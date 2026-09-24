d9c0c2936613c68aade4881f5b373ee7dd2b02c6
tauheedurrahman <tauheedurrahman115@gmail.com>
2026-09-24 22:39:18 +0500

    Update Dart learning files and README

---
+++ bin/Assigment.dart
@@ -0,0 +1,127 @@
+void main(){
+  String Myname = "Tauheed";
+  String MyfatherName ="Hidayat khan";
+  String MyBrotherName = "Khizar";
+  print("My Name is $Myname and my father name is $MyfatherName and my brother name is $MyBrotherName");
+  print("____________________________________");
+  /*
+  Squre shape
+   */
+
+  print("* * * * ");
+  print("* * * * ");
+  print("* * * * ");
+  /*
+  Rectangle shape
+   */
+  print("____________________________________");
+  print("* * * * * * * *");
+  print("* * * * * * * * ");
+  print("* * * * * * * *");
+  print("* * * * * * * *");
+  print("____________________________________");
+  /*
+  Triangle Shape
+   */
+
+  print("* ");
+  print("* * ");
+  print("* * *");
+  print("* * * * ");
+  print("* * * * * ");
+  print("* * * * * * ");
+  print("____________________________________");
+  /*
+  Pentagon Shape
+   */
+  print("    *    ");
+  print("   * *   ");
+  print("  *   * ");
+  print(" *     * ");
+  print("* * * * *   ");
+  print("____________________________________");
+  /*
+  Circale Shape
+   */
+  print("   ***   ");
+  print("  *   *  ");
+  print("*       *");
+  print("*       *");
+  print("  *    * ");
+  print("   ***   ");
+  print("____________________________________");
+  /*
+  Two numbers stores in 3rd Varibale
+   */
+  int a = 10;
+  int b = 20;
+  int sum = a+b;
+  print("$sum");
+  print("____________________________________");
+/*
+Adding Two numbers
+ */
+  int L = 10;
+  int T = 30;
+  print(L+T);
+  print("____________________________________");
+  double C = 10;
+  double D = 30;
+  print(C*D);
+  print("____________________________________");
+  /*
+Arithmethic Operaters
+   */
+  print(a+b);
+  print(a-b);
+  print(a*b);
+  print(a/b);
+  print(a%b);
+  print("____________________________________");
+  /*
+  Feets into Meters and maters into Kelometers
+   */
+  double feet = 15.0;
+  double meters = feet * 0.3048;
+  double kelometers = meters / 1000;
+  print("The Feet $feet into Meters is $meters and the Meters into keloMeters is $kelometers ");
+  print("____________________________________");
+  /*
+  clacuis to Fahreneit
+   */
+
+  /* double calsuis = 30;
+  double farhrenheit = ( calsuis * 9/5) + 32;
+  print(farhrenheit);
+  */
+  print("____________________________________");
+
+  /*
+  Fahreneit into clacuis
+   */
+  double farhrenheit = 77;
+  double calsuis =(farhrenheit - 32) * 5/9;
+  print(calsuis);
+  print("____________________________________");
+  /*
+  area of circle
+   */
+  double radius = 5;
+  double area = 3.14 * radius * radius;
+  print(area);
+  print("____________________________________");
+  /*
+  Area of Squre
+   */
+  double side = 5;
+  double area1 = side*side;
+  print(area1);
+  print("____________________________________");
+  /*
+  Area of rectangular
+   */
+  double length = 10 ;
+  double width1 = 5;
+  double area2 = length * width1;
+  print(area2);
+}
